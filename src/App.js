c067ee2f8742684adc4229e6d8112c27d51be7d1
drew ponce <drew@drews-mbp.home>
2021-06-01 18:32:27 -0700

    second-commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test Tailwind-React app.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/drewdevvv"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my Github
         </a>
       </header>
     </div>
